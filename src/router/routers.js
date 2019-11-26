481a57476a664ff577ce89bb2a586d7fb20d38cf
lxy402 <linxingyi5@163.com>
2019-11-26 17:14:31 +0800

    fix:去除组织树部分

--- src/router/routers.js
+++ src/router/routers.js
@@ -51,26 +51,6 @@ export default [
     ]
   },
   {
-    path: '/message',
-    name: 'message',
-    component: Main,
-    meta: {
-      hideInBread: true,
-      hideInMenu: true
-    },
-    children: [
-      {
-        path: 'message_page',
-        name: 'message_page',
-        meta: {
-          icon: 'md-notifications',
-          title: '消息中心'
-        },
-        component: () => import('@/view/single-page/message/index.vue')
-      }
-    ]
-  },
-  {
     path: '/components',
     name: 'components',
     meta: {
@@ -114,15 +94,6 @@ export default [
           title: '可拖拽抽屉'
         },
         component: () => import('@/view/components/drag-drawer')
-      },
-      {
-        path: 'org_tree_page',
-        name: 'org_tree_page',
-        meta: {
-          icon: 'ios-people',
-          title: '组织结构树'
-        },
-        component: () => import('@/view/components/org-tree')
       },
       {
         path: 'tree_table_page',
